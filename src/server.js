fb4f77435f46b56ec9e1adbffc4773a546a194d8
Dustin Hoffman <dwhoffman@hotmail.com>
2012-03-02 04:42:53 +0000

    "Updated to create the server instance that logs requests and writes back simple json."

--- src/server.js
+++ src/server.js
@@ -1,2 +1,11 @@
 var http = require('http');
 
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    console.log('Request -> URL: %s  METHOD: %s', req.url, req.method);
+    var response = {'result': 'hello world'};
+    console.log('Sending Response -> %s', JSON.stringify(response));
+    res.write(JSON.stringify(response));
+    res.end();
+}).listen(process.env.PORT);
+console.log('Server running on port %d', process.env.PORT);
